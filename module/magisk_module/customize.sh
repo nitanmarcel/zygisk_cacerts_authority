ede312025a0627640f93fcf137a05865fd3946db
Marcel Alexandru Nitan <nitan.marcel@protonmail.com>
2024-03-08 16:49:06 +0200

    merge with custom-certificate-authorities

--- module/magisk_module/customize.sh
+++ module/magisk_module/customize.sh
@@ -2,17 +2,14 @@ SKIPUNZIP=1
 
 enforce_install_from_app() {
   if $BOOTMODE; then
-    ui_print "- Installing from Magisk app"
+    ui_print "- Installing from Magisk/KernelSU app"
   else
     ui_print "*********************************************************"
     ui_print "! Install from recovery is NOT supported"
-    ui_print "! Recovery sucks"
-    ui_print "! Please install from Magisk"
+    ui_print "! Please install from Magisk/KernelSU"
     abort "*********************************************************"
   fi
 }
-
-enforce_install_from_app
 
 ui_print "- Extracting module files"
 unzip -o "$ZIPFILE" -x 'META-INF/*' -d $MODPATH >&2
@@ -20,7 +17,5 @@ unzip -o "$ZIPFILE" -x 'META-INF/*' -d $MODPATH >&2
 if [ "$API" -lt 34 ]; then
   rm -rf "$MODPATH/zygisk"
 fi
-
-REPLACE="/system/etc/security/cacerts"
 
 set_perm_recursive "$MODPATH" 0 0 0755 0644
